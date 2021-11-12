26ed1e0f2de235ba86ecb75cdb58a70a64a2a532
yojenkins <yojenkins@users.noreply.github.com>
2021-11-12 00:54:35 +0000

    Design Token Update

---
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -0,0 +1,40 @@
+
+//
+// StyleDictionaryColor.swift
+//
+
+// Do not edit directly
+// Generated on Fri, 12 Nov 2021 00:54:34 GMT
+
+
+import UIKit
+
+public enum StyleDictionaryColor {
+    public static let colorsMultipleFills0 = UIColor(red: 0.251, green: 1.000, blue: 0.729, alpha: 1)
+    public static let colorsMultipleFills1 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.1)
+    public static let colorsSingleBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
+    public static let colorsSpecialCharacters = UIColor(red: 0.251, green: 0.875, blue: 0.314, alpha: 1) /* Emoji */
+    public static let colorsSpecialCharactersNderung = UIColor(red: 0.204, green: 0.337, blue: 0.686, alpha: 1)
+    public static let darkBackground = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let dashedOutsideStroke = rgba(0, 0, 0, 1)
+    public static let dropShadowSingleColor = rgba(0, 0, 0, 0.25)
+    public static let gradientMultiple0Stops0Color = rgba(255, 184, 0, 1)
+    public static let gradientMultiple0Stops1Color = rgba(255, 184, 0, 1)
+    public static let gradientMultiple1Stops0Color = rgba(255, 255, 255, 1)
+    public static let gradientMultiple1Stops1Color = rgba(255, 255, 255, 1)
+    public static let gradientMultiple2Stops0Color = rgba(207, 48, 48, 1)
+    public static let gradientMultiple2Stops1Color = rgba(255, 255, 255, 1)
+    public static let gradientMultiple3Stops0Color = rgba(74, 79, 204, 1)
+    public static let gradientMultiple3Stops1Color = rgba(255, 255, 255, 1)
+    public static let gradientSingleWithMultipleColorStopsStops0Color = rgba(255, 184, 0, 1)
+    public static let gradientSingleWithMultipleColorStopsStops1Color = rgba(255, 138, 0, 1)
+    public static let gradientSingleWithMultipleColorStopsStops2Color = rgba(255, 46, 0, 1)
+    public static let gradientSingleWithMultipleColorStopsStops3Color = rgba(255, 0, 0, 1)
+    public static let innerShadowMultiple0Color = rgba(0, 0, 0, 0.25)
+    public static let innerShadowMultiple1Color = rgba(0, 0, 0, 1)
+    public static let innerShadowMultiple2Color = rgba(0, 0, 0, 0.25)
+    public static let lightBackground = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let singleStroke = rgba(0, 0, 0, 1)
+    public static let singleStyleStroke = rgba(4, 74, 255, 1)
+    public static let unsupportedMultipleBordersStroke = rgba(255, 230, 0, 1)
+}
