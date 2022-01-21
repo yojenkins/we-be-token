5a6affd1fd9bd615cbf79d107d00aff487635e25
Jenkins <evan.jenkins@remax.com>
2022-01-20 21:42:21 -0800

    build flat tokens

--- scripts/build.js
+++ scripts/build.js
@@ -2,12 +2,20 @@ const StyleDictionary = require("style-dictionary");
  
 const tokenSets = ['color', 'size', 'typography']
 
-tokenSets.forEach((name) => {
-  StyleDictionary.registerTransformGroup({
-    name: "react-native",
-    transforms: ["attribute/cti", "name/ti/constant"],
-  });
+StyleDictionary.registerTransform({
+  type: 'value',
+  name: 'testTransform',
+  transitive: true,
+  matcher: (token) => token.name.toLowerCase().includes('alias'),
+  transformer: token => token.value.value
+})
 
+StyleDictionary.registerTransformGroup({
+  name: "react-native",
+  transforms: ["name/cti/constant", 'testTransform'],
+});
+
+tokenSets.forEach((name) => {
   StyleDictionary.extend({
     source: [`tokens/${name}/**/*.json`],
     platforms: {
